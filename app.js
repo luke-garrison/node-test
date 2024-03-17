bb76f36dd2d84fcf4e436c439543881be461228d
luke-garrison <lukeagarrison@gmail.com>
2024-03-17 01:09:44 -0500

    testing

--- app.js
+++ app.js
@@ -1,24 +1,10 @@
-const http = require('http');
-const target_ip = '18.191.213.254'; 
-const port = 3000;
+const Docker = require('dockerode');
+const docker = new Docker(); 
 
-const server = http.createServer((req, res) => {
-  const options = {
-    hostname: target_ip,
-    port: 3000,
-    path: req.url,
-    method: req.method,
-    headers: req.headers
-  };
+async function startExistingContainer() {
+  const container = docker.getContainer('metabase'); // Assuming 'my-container' exists
+  await container.start();
+  console.log('Container started!');
+}
 
-  const proxyReq = http.request(options, (proxyRes) => {
-    res.writeHead(proxyRes.statusCode, proxyRes.headers);
-    proxyRes.pipe(res, { end: true }); 
-  });
-
-  req.pipe(proxyReq, { end: true }); 
-});
-
-server.listen(port, () => {
-  console.log(`Proxy server running at http://localhost:${port}/`);
-});
+startExistingContainer(); 
